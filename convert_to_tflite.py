bbde6cee46857965f52de2e99a2a99046816d8e0
Abenezer Seifu <abenezerseifu123@gmail.com>
2025-01-03 22:37:34 +0300

    Changed model to blood_sweat_tears

--- convert_to_tflite.py
+++ convert_to_tflite.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Load the Keras H5 model
-h5_model_path = "best_model.h5"
+h5_model_path = "blood_sweat_tears.h5"
 model = tf.keras.models.load_model(h5_model_path)
 
 # Convert the model to TensorFlow Lite format
@@ -9,7 +9,7 @@ converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 # Save the TensorFlow Lite model
-tflite_model_path = "best_model.tflite"
+tflite_model_path = "blood_sweat_tears.tflite"
 with open(tflite_model_path, "wb") as f:
     f.write(tflite_model)
 
